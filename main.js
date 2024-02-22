86023d45d87ea5ac75cf187503282798e7f879c0
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-02-22 23:18:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+const timer = document.getElementById("timer");
+const start = document.getElementById("start");
+const stop =document.getElementById("stop");
+const restart = document.getElementById("reset");
+
+let interval;
+let timeLeft = 1500;
+
+function updateTimer() {
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60;
+    let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    timer.innerHTML = formattedTime
+}
+
+function startTimer() {
+    interval = setInterval(() => {
+        timeLeft--;
+        updateTimer();
+        if(timeLeft === 0) {
+            clearInterval(interval);
+            alert("Time's up!");
+            timeLeft = 1500;
+            updateTimer();
+        }
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(interval);
+}
+
+function resetTimer() {
+    clearInterval(interval);
+    timeLeft = 1500;
+    updateTimer();
+}
+
+start.addEventListener("click", startTimer);
+stop.addEventListener("click", stopTimer);
+restart.addEventListener("click", resetTimer);
+
